70a4e4bd57db52b6b791088782815f229f9a7ed6
c-rothschild <158000738+c-rothschild@users.noreply.github.com>
2026-09-10 10:34:40 -0600

    updated tau_run to do multiple tests and minor fixes to build.sh

--- src/build.sh
+++ src/build.sh
@@ -16,9 +16,13 @@ export TAU_OPTIONS="-optCompInst -optTauSelectFile=select.tau -optVerbose"
 
 CC=tau_cc.sh
 #$CC -Wall -Wextra -std=c11 -DTAU_ENABLED -O2 -o network_tau_v1_O2 main.c   networkV1.c mnist_loader.c sigFuncs.c -lm
+rm -f ./*.o
 $CC -Wall -Wextra -std=c11 -DTAU_ENABLED -O3 -o networkV1 main.c networkV1.c mnist_loader.c sigFuncs.c -lm
+rm -f ./*.o
 $CC -Wall -Wextra -std=c11 -DTAU_ENABLED -O3 -o networkV2 main.c networkV2.c mnist_loader.c sigFuncs.c -lm
+rm -f ./*.o
 $CC -Wall -Wextra -std=c11 -DTAU_ENABLED -O3 -fopenmp -o networkV3 main.c networkV3.c mnist_loader.c sigFuncs.c -lm
+rm -f ./*.o
 
 for b in network_tau_v1 network_tau_v1_O2 network_tau_v2; do
     printf '%s: %s TAU symbols\n' "$b" "$(nm "$b" | grep -c Tau_)"
